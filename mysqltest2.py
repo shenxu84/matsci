6b38d202d8be3f3883655219a0f8d7f43055b7ad
shenxu84 <166746132+shenxu84@users.noreply.github.com>
2024-04-11 14:04:55 -0400

    Update mysqltest2.py

--- mysqltest2.py
+++ mysqltest2.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
 from matminer.datasets import load_dataset
 from sklearn.ensemble import RandomForestRegressor
